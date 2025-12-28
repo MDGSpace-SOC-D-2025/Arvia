a956f7f236d2fb5cf8a1cb1ba4b573dc5df79f7d
swdha <swdha_g@cs.iitr.ac.in>
2025-12-29 00:44:08 +0530

    symptom file updated for location

--- app/routes/symptom.py
+++ app/routes/symptom.py
@@ -5,8 +5,8 @@ from app.services.symptom_service import analyze_symptoms
 router = APIRouter()
 
 
-@router.post("/check-symptoms", response_model=SymptomResponse)
-async def check_symptoms(request: SymptomRequest):
+@router.post("/check-symptoms", response_model=SymptomResponse)   # defining api endpoint
+async def check_symptoms(request: SymptomRequest): # receive http request
     """
     Symptom checker endpoint.
     
